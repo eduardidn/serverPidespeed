4e5fcd31c1dd5ccaa0b0f1d856163359269ff439
eduardo <eduardidn@gmail.com>
2019-06-26 13:34:24 -0400

    primera subida al servidor

--- build/index.js
+++ build/index.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const morgan_1 = __importDefault(require("morgan"));
+//const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
 const functions_1 = __importDefault(require("./functions"));
 //ROUTES
@@ -21,7 +21,7 @@ class Server {
     }
     config() {
         this.app.set('port', process.env.PORT || 3000);
-        this.app.use(morgan_1.default('dev'));
+        //this.app.use(morgan_1.default('dev'));
         this.app.use(cors_1.default());
         this.app.use(express_1.default.json());
         this.app.use(express_1.default.urlencoded({ extended: false }));
