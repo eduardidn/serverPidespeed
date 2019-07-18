5a850d7de4c9ff046bbc884787c88ab62aec4b34
eduardo <eduardidn@gmail.com>
2019-07-18 17:09:33 -0400

    ...

--- build/index.js
+++ build/index.js
@@ -6,8 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 //const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
+const functions_1 = __importDefault(require("./functions"));
 //ROUTES
 const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
+const testRoutes_1 = __importDefault(require("./routes/testRoutes"));
 const empresasRoutes_1 = __importDefault(require("./routes/empresasRoutes"));
 const productosRoutes_1 = __importDefault(require("./routes/productosRoutes"));
 const sesionesRoutes_1 = __importDefault(require("./routes/sesionesRoutes"));
@@ -38,7 +40,7 @@ class Server {
     routes() {
         this.app.use('/', indexRoutes_1.default);
         this.app.use('/sesiones', sesionesRoutes_1.default);
-        //this.app.use('/api',func.verifyToken, testRoutes);
+        this.app.use('/api', functions_1.default.verifyToken, testRoutes_1.default);
         this.app.use('/api/empresas', empresasRoutes_1.default);
         this.app.use('/api/productos', productosRoutes_1.default);
         this.app.use('/api/usuarios', usuariosRoutes_1.default);
