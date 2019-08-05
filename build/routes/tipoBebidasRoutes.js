39f8db44d1818a1bc93d617447f1fc5808a4ced5
eduardo <eduardidn@gmail.com>
2019-08-05 14:20:26 -0400

    nuevas tablas

---
+++ build/routes/tipoBebidasRoutes.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const tipoBebidasController_1 = __importDefault(require("../controllers/tipoBebidasController"));
+class TipoBebidasRoutes {
+    constructor() {
+        this.router = express_1.Router();
+        this.config();
+    }
+    config() {
+        this.router.get('/', tipoBebidasController_1.default.list);
+        this.router.get('/:id', tipoBebidasController_1.default.getOne);
+        this.router.post('/', tipoBebidasController_1.default.create);
+        this.router.put('/:id', tipoBebidasController_1.default.update);
+        this.router.delete('/:id', tipoBebidasController_1.default.delete);
+    }
+}
+exports.default = new TipoBebidasRoutes().router;
