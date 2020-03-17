c124c1aa7a570d15f7de788544e8a0a1aeed22cc
eduardo <eduardidn@gmail.com>
2020-03-17 14:06:08 -0400

    commit

--- build/routes/SaboresRoutes.js
+++ build/routes/SaboresRoutes.js
@@ -13,6 +13,7 @@ class SaboresRoutes {
     config() {
         this.router.get('/:id/:tipo', SaboresController_1.default.list);
         this.router.get('/get/one/:id', SaboresController_1.default.getOne);
+        this.router.put('/byIds/:id', SaboresController_1.default.updateByIds);
         this.router.put('/:id', SaboresController_1.default.update);
         this.router.post('/', SaboresController_1.default.create);
         this.router.delete('/:id', SaboresController_1.default.delete);
