09e027fe37ad5bfd10c57132236f5ca820dd8af3
eduardo <eduardidn@gmail.com>
2020-08-18 16:17:51 -0400

    nuevos sockets

--- build/index.js
+++ build/index.js
@@ -133,15 +133,18 @@ class Server {
             console.log('new connection', socket.id);
             socket.on('pedido:actualizado', (data) => {
                 this.io.sockets.emit('pedido:actualizado', data);
-                console.log('pedido actualizado');
             });
             socket.on('pedido:nuevoEmpresa', (data) => {
                 this.io.sockets.emit('pedido:nuevoEmpresa', data);
-                console.log('pedido nuevo empresa');
             });
             socket.on('pedido:nuevo', (data) => {
                 this.io.sockets.emit('pedido:nuevo', data);
-                console.log('pedido nuevo');
+            });
+            socket.on('actualizar:pedidos', (data) => {
+                socket.broadcast.emit('actualizar:pedidos', data);
+            });
+            socket.on('actualizar:pedidosEmpresa', (data) => {
+                socket.broadcast.emit('actualizar:pedidosEmpresa', data);
             });
         });
     }
