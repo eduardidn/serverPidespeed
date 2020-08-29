8c31ee8061cf008de8b8e1c46a7fc1b5a8873c91
eduardo <eduardidn@gmail.com>
2020-08-29 01:21:50 -0400

    nueva bd

--- build/keys.js
+++ build/keys.js
@@ -8,9 +8,9 @@ exports.default = {
         database: 'pidexpress'
     } */
     database: {
-        host: "34.232.202.24",
-        user: 'root',
-        password: 'Pidespeed2020$',
+        host: "31.220.56.153",
+        user: 'eduardidn',
+        password: 'Eduar.07',
         database: 'b4bdttx6r1sgazwbqffd'
     }
 };
